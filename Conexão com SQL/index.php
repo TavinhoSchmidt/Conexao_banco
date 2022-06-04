4606a644a6c922ee8971854cb3fd598c48d75cc8
TavinhoSchmidt <83095377+TavinhoSchmidt@users.noreply.github.com>
2022-06-03 23:25:37 -0300

    Add files via upload

---
+++ Conexão com SQL/index.php
@@ -0,0 +1,13 @@
+<?php
+include './config.php';
+$query = $con -> query("Select nome, sobrenome from usuario");
+$dbs = $con -> query("show databases"); 
+while($result = mysqli_fetch_array($dbs)){
+    print_r($result[0]);
+    echo "</br>";
+}
+echo "<ul>";
+while($row = $query -> fetch_object()){
+    echo "<li>".$row->nome." " .$row-> sobrenome."</li>";
+}
+echo "</ul>";
