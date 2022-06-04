51d03d8ddb21d130cbc50df2e8d1cef43730b1ee
TavinhoSchmidt <83095377+TavinhoSchmidt@users.noreply.github.com>
2022-06-03 23:39:02 -0300

    Update config.php

--- Conexão com SQL/config.php
+++ Conexão com SQL/config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "usuarios";
-$con = new mysqli($host, $user, $password, $db);
+  $host = "localhost";
+  $user = "root";
+  $password = "";
+  $db = "usuarios";
+  $con = new mysqli($host, $user, $password, $db);
 
